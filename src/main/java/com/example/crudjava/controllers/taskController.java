2203bfaed5bbab7ecef4aeb1ea63a7613b7488fe
IceEter07 <alberto.cuadros90@gmail.com>
2023-08-16 19:34:41 -0600

    Creación del modelo para las tareas

--- src/main/java/com/example/crudjava/controllers/taskController.java
+++ src/main/java/com/example/crudjava/controllers/taskController.java
@@ -1,4 +1,20 @@
 package com.example.crudjava.controllers;
 
+import com.example.crudjava.models.tareas;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/tareas")
 public class taskController {
+
+    @GetMapping
+    public tareas getTask(){
+        tareas task = new tareas();
+        task.setId(1L);
+        task.setDescripcion("Hlalallalasf");
+        task.setEstatus("NASHDSADF");
+        return task;
+    }
 }
---
+++ src/main/java/com/example/crudjava/models/tareas.java
@@ -0,0 +1,23 @@
+package com.example.crudjava.models;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tareas")
+
+public class tareas {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter @Setter @Column(name = "id")
+    private Long id;
+
+    @Getter @Setter @Column(name = "descripcion")
+    private String descripcion;
+
+    @Getter @Setter @Column(name = "estatus")
+    private String estatus;
+}
+
